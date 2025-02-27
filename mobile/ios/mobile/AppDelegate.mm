2f3b0f7edb9ca396efef034126956392bfdd504c
vansh.gupta <vansh.gupta@geekyants.com>
2025-02-27 16:07:23 +0530

    updated slug

--- mobile/ios/mobile/AppDelegate.mm
+++ mobile/ios/mobile/AppDelegate.mm
@@ -58,5 +58,19 @@
 {
   return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
 }
+//added by me
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
 
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
 @end
